3fc05fd31be74b6d47d29568236f5def83556765
Shahd Elhlwaney <ShahdElhlwaney@example.com>
2024-11-23 06:30:45 +0200

    Generate random dice roll and display it

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 'use strict';
+const diceEl=document.querySelector('.dice');
+const btnDice=document.querySelector('.btn--roll');
+const btnNew=document.querySelector('.btn--new');
+const btnHold=document.querySelector('.btn--hold');
+const score0El=document.querySelector('.score-player0');
+const score1El=document.querySelector('.score-player1');
+const currentScore0El=document.querySelector('.currentscore-player0');
+const currentScore1El=document.querySelector('.currentscore-player1');
+
+
+document.querySelector('.btn--roll').addEventListener('click',
+    function (){
+        const randomNumber=Math.trunc(Math.random()*6)+1;
+        diceEl.src=`dice-${randomNumber}.png`;
+        diceEl.classList.remove('hidden');
+    });
+    
