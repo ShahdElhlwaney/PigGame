540c3aa834d9ffe4cae254860ea828daa515019c
Shahd Elhlwaney <ShahdElhlwaney@example.com>
2024-11-23 09:07:45 +0200

    Add current scores, switch between players and change the background-color for the active player

--- script.js
+++ script.js
@@ -7,12 +7,30 @@ const score0El=document.querySelector('.score-player0');
 const score1El=document.querySelector('.score-player1');
 const currentScore0El=document.querySelector('.currentscore-player0');
 const currentScore1El=document.querySelector('.currentscore-player1');
+const player0El=document.querySelector('.player0');
+const player1El=document.querySelector('.player1');
 
-
+let playerActive=0;
+let currentScore=0;
+const currentScoreEls=[currentScore0El,currentScore1El];
 document.querySelector('.btn--roll').addEventListener('click',
     function (){
-        const randomNumber=Math.trunc(Math.random()*6)+1;
-        diceEl.src=`dice-${randomNumber}.png`;
+        const dice=Math.trunc(Math.random()*6)+1;
+        diceEl.src=`dice-${dice}.png`;
         diceEl.classList.remove('hidden');
+        if(dice!==1)
+        {
+            currentScore+=dice;
+            document.querySelector(`.currentscore-player${playerActive}`).textContent=currentScore;
+        }
+        else
+        {
+            document.querySelector(`.currentscore-player${playerActive}`).textContent=0;
+            currentScore=0;
+            playerActive=playerActive===0?1:0;
+            player0El.classList.toggle('playerActive');
+            player1El.classList.toggle('playerActive');
+        }
+
     });
-    
+
